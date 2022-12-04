914e88b01a9870bf15a99d28ed8f4c1fae01d8c2
Reza Safaei <rezasafaei70@gmail.com>
2022-12-04 09:40:36 +0330

    create auto folders log and database

--- settings.py
+++ settings.py
@@ -2,5 +2,14 @@
 
 from dotenv import dotenv_values
 import os
+
+isExist_logs = os.path.exists('logs')
+isExist_databse = os.path.exists('database')
+
+if not isExist_databse:
+    os.makedirs('database')
+if not isExist_logs:
+    os.makedirs('logs')
+
 config = dotenv_values(".env")
 
